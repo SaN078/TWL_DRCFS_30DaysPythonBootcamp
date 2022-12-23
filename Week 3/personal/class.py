e1a993d6437144e747ae2ae9a43ac80d2bed615f
SaN078 <sangeetraut29@gmail.com>
2022-12-23 19:55:20 +0545

    oop surfacials

---
+++ Week 3/personal/class.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+
+
+
+class Workshop:
+    var = 1
+
+
+    '''
+    abc
+    '''
+
+var1 = int('1')
+
+print(Workshop().var)
+
+
+
+class animal:
+    name = 'cleo'
+    age = 3
+
+
+    def __init__(self):
+        self.find('a', class_ = 'abc')
+
+
+a = BeautifulSoup('html_str')
+animal()
+a.find
+print(animal.name)
