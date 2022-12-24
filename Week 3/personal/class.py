10c48c83d1668073974a9e2c097e05f3ad9a24a0
SaN078 <sangeetraut29@gmail.com>
2022-12-24 06:26:52 +0545

    class and objects initialized

--- Week 3/personal/class.py
+++ Week 3/personal/class.py
@@ -14,7 +14,7 @@ var1 = int('1')
 
 print(Workshop().var)
 
-
+'''
 
 class animal:
     name = 'cleo'
@@ -24,8 +24,20 @@ class animal:
     def __init__(self):
         self.find('a', class_ = 'abc')
 
-
+'''
+'''
 a = BeautifulSoup('html_str')
 animal()
 a.find
 print(animal.name)
+
+'''
+class her:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+h1 = her('San',69)
+
+print(h1.name)
+print(h1.age)
